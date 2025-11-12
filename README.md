8a0a21c4a922742400f7587ef7abdf9306cdf303
aymen-bc <aymenbencheikh789@gmail.com>
2025-11-12 13:00:05 +0100

    fix bugs in database

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Gestion_Dons_Sang
+
+to create the database: 
+open command line and run this command " sudo mariadb -u admin --password="admin" < query.sql "
--- query.sql
+++ query.sql
@@ -3,46 +3,52 @@ USE don_sang;
 
 CREATE TABLE IF NOT EXISTS donneurs(
     id_donneur INT PRIMARY KEY AUTO_INCREMENT,
-    cin varchar(50) UNIQUE NOT NULL,
-    groupe_sanguin enum('A', 'B', 'AB', 'O') NOT NULL,
-    rhesus enum('-', '+') NOT NULL
-)
+    cin VARCHAR(50) UNIQUE NOT NULL,
+    groupe_sanguin ENUM('A', 'B', 'AB', 'O') NOT NULL,
+    rhesus ENUM('-', '+') NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS centres_collecte (
     id_centre INT PRIMARY KEY AUTO_INCREMENT
-)
+);
 
 CREATE TABLE IF NOT EXISTS utilisateurs (
     id_utilisateur INT PRIMARY KEY AUTO_INCREMENT,
-    role enum('Admin', 'Médecin', 'Secrétaire') NOT NULL,
-    FOREIGN KEY (id_centre) REFERENCES centres_collecte(id_centre) NOT NULL,
-    password varchar(200) NOT NULL,
-)
+    role ENUM('Admin', 'Médecin', 'Secrétaire') NOT NULL,
+    id_centre INT NOT NULL,
+    FOREIGN KEY (id_centre) REFERENCES centres_collecte(id_centre),
+    password VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS dons (
     id_don INT PRIMARY KEY AUTO_INCREMENT,
-    FOREIGN KEY (id_donneur) REFERENCES donneurs(id_donneur) NOT NULL,
-    FOREIGN KEY (id_centre) REFERENCES centres_collecte(id_centre) NOT NULL,
-    status enum('EN STOCK', 'UTILISÉ', 'REJETÉ') NOT NULL;
-)
+    id_donneur INT NOT NULL,
+    id_centre INT NOT NULL,
+    FOREIGN KEY (id_donneur) REFERENCES donneurs(id_donneur),
+    FOREIGN KEY (id_centre) REFERENCES centres_collecte(id_centre),
+    status ENUM('EN STOCK', 'UTILISÉ', 'REJETÉ') NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS tests_don (
-    id_test INT PRIMARY KEY AUTOINCREMENT ,
-    FOREIGN KEY (id_don) REFERENCES dons(id_don) UNIQUE NOT NULL,
-    FOREIGN KEY (id_centre) REFERENCES centres_collecte(id_centre) NOT NULL,
-    est_conforme BOOL NOT NULL;
-)
+    id_test INT PRIMARY KEY AUTO_INCREMENT,
+    id_don INT UNIQUE NOT NULL,
+    id_centre INT NOT NULL,
+    FOREIGN KEY (id_don) REFERENCES dons(id_don),
+    FOREIGN KEY (id_centre) REFERENCES centres_collecte(id_centre),
+    est_conforme BOOL NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS transfusions (
-    id_transfusion INT PRIMARY KEY AUTOINCREMENT,
-    FOREIGN KEY (id_don) REFERENCES dons(id_don) UNIQUE NOT NULL,
-    date_trnasfusion DATE NOT NULL,
-    hopital_recepteur varchar(150) NOT NULL
-)
+    id_transfusion INT PRIMARY KEY AUTO_INCREMENT,
+    id_don INT UNIQUE NOT NULL,
+    FOREIGN KEY (id_don) REFERENCES dons(id_don),
+    date_transfusion DATE NOT NULL,
+    hopital_recepteur VARCHAR(150) NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS besoins (
     id_besoin INT PRIMARY KEY AUTO_INCREMENT,
-    groupe_sanguin enum('A', 'B', 'AB', 'O') NOT NULL,
-    rhesus enum('-', '+') NOT NULL,
-    niveau_alerte enum('URGENT', 'CRITIQUE', 'NORMALE') NOT NULL
-)
+    groupe_sanguin ENUM('A', 'B', 'AB', 'O') NOT NULL,
+    rhesus ENUM('-', '+') NOT NULL,
+    niveau_alerte ENUM('URGENT', 'CRITIQUE', 'NORMALE') NOT NULL
+);
